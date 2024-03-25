c2efc7edb288f3addacb8978202e988661a33397
LascanoAldahir <aldahirlascano@gmail.com>
2024-03-25 00:31:54 -0500

    .

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -11,7 +11,7 @@ const app = express()
 dotenv.config()
 
 // Configuraciones 
-app.set('PORT',process.env.port || 3000)
+app.set('PORT',process.env.PORT || 3000)
 app.use(cors())
 
 // Middlewares 
